9388d5166eab6b7fa2e150a88d3a6f6be5442b1e
Mats Gjerset Rødseth <matsroedseth@hotmail.no>
2018-03-21 00:26:41 +0100

    paradise-switch changes theme

--- snart-helg-app/src/app/app.component.ts
+++ snart-helg-app/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ParadiseModeService } from './core/services/paradisemode.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isParadiseMode: Observable<boolean>;
+
+  constructor(private paradiseModeService: ParadiseModeService) { }
+
+  ngOnInit() {
+    this.isParadiseMode = this.paradiseModeService.isParadiseMode;
+  }
 }
